9ecb039fbb0c519ef4c9b73622daa0dc52aa7041
akshanator <akbfc@icloud.com>
2015-06-15 19:42:27 -0400

    bleh

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: => 'realfootball.herokuapp.com' }
+  config.action_mailer.default_url_options =  { :host 'realfootball.herokuapp.com' }
+  
 end
