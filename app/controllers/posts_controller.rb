91b817ce94fd8f91b0c5b95b755cf92879fc7575
akshanator <akbfc@icloud.com>
2015-06-17 18:46:36 -0400

    did some mo posts stuff

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
+  before_action :current_user, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -44,6 +45,11 @@ class PostsController < ApplicationController
   private
     def set_post
       @post = Post.find(params[:id])
+    end
+
+    def correct_user
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to posts_path, noitce: "Not authorized to edti this post" if @post.nil?
     end
 
     def post_params
