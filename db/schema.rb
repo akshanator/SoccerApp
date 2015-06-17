81e691624748b53ad8ba66db7dca122e0db64916
akshanator <akbfc@icloud.com>
2015-06-17 06:58:18 -0400

    changed some players stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150616205357) do
+ActiveRecord::Schema.define(version: 20150617105731) do
 
   create_table "players", force: :cascade do |t|
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "desctiption"
+    t.string   "description"
   end
 
   create_table "posts", force: :cascade do |t|
