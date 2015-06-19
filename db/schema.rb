a8d9b6fb4f24d6339b7fd340ad87595ab3c21cb0
akshanator <akbfc@icloud.com>
2015-06-19 07:47:50 -0400

    masonry to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150619021126) do
+ActiveRecord::Schema.define(version: 20150619114714) do
 
   create_table "players", force: :cascade do |t|
     t.datetime "created_at",           null: false
@@ -28,14 +28,18 @@ ActiveRecord::Schema.define(version: 20150619021126) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "description"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
     t.text     "body"
     t.integer  "user_id"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
