bb0d88679ea38d5ba2d04cabce1fbf17435975e8
akshanator <akbfc@icloud.com>
2015-06-21 09:43:28 -0400

    changed home page to posts index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :players
   resources :posts
   devise_for :users
-  root 'pages#home'
+  root 'posts#index'
   get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
