9d43b51457a8f520b7844846670144aebc20a120
akshanator <akbfc@icloud.com>
2015-06-21 21:53:11 -0400

    added will_paginate to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :current_user, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.all.order("created_at DESC")
+    @posts = Post.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
   end
 
   def show
